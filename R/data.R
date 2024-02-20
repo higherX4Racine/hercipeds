056bbef74000e610f3e6565a461f51f0e10efc43
Ben Taft <ben.taft@career2cradle.org>
2024-02-20 17:08:31 -0600

    defined data tables for subject area codes

--- R/data.R
+++ R/data.R
@@ -16,7 +16,7 @@
 #' Integer codes for different combinations of first-time, full-time, and financial aid statuses
 #' 
 #' @format ## `OMCHRT`
-#' An object of class `spe_tbl_df` (inhertis from `tbl_df`, `tbl`, `data.frame`), with 15 rows and 4 columns.
+#' An object of class `spec_tbl_df` (inherits from `tbl_df`, `tbl`, `data.frame`), with 15 rows and 4 columns.
 #' \describe{
 #' \item{OMCHRT}{<int> Code value for cohort type}
 #' \item{Entry Status}{<chr> All, First-time, or Not-first-time}
@@ -48,3 +48,44 @@
 #'  
 #' @source <https://nces.ed.gov/ipeds/datacenter/DataFiles.aspx>
 "CORNELLS"
+
+#' Very broad categories of subject areas for post-secondary credentials.
+#' @format ## `CIP_FAMILIES`
+#' 
+#' An object of class \code{spec_tbl_df} (inherits from \code{tbl_df}, \code{tbl}, \code{data.frame}) with 50 rows and 3 columns.
+#' 
+#' ### Code
+#' 
+#' A character vector of two-digit numeric codes
+#' 
+#' ### Title
+#' 
+#' A somewhat short description of the subject area. IN ALL CAPS FOR DRAMA
+#' 
+#' ### Definition
+#' 
+#' A somewhat longer description of each subject area. In sentence case.
+#' 
+#' @source <https://nces.ed.gov/ipeds/cipcode/resources.aspx>
+"CIP_FAMILIES"
+
+#' Narrow categories of subject areas for post-secondary credentials.
+#' @format ## `CIP_PROGRAMS`
+#' 
+#' An object of class \code{spec_tbl_df} (inherits from \code{tbl_df}, \code{tbl}, \code{data.frame}) with 473 rows and 3 columns.
+#' 
+#' ### Code
+#' 
+#' A character vector of two-digit numeric codes
+#' 
+#' ### Title
+#' 
+#' A somewhat short description of the subject area. In title case.
+#' 
+#' ### Definition
+#' 
+#' A somewhat longer description of each subject area. In sentence case.
+#' 
+#' @source <https://nces.ed.gov/ipeds/cipcode/resources.aspx>
+"CIP_PROGRAMS"
+
