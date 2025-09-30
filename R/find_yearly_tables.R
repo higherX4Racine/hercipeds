285d79eb4d8dadbd77920d279e67356704acf289
Ben Taft <ben.taft@career2cradle.org>
2025-09-30 17:01:28 -0500

    added documentation for finding all tables that match a pattern

--- R/find_yearly_tables.R
+++ R/find_yearly_tables.R
@@ -1,8 +1,28 @@
 ## Copyright (C) 2025 by Higher Expectations for Racine County
 
+#' Search a path for files with IPEDS-type names
+#'
+#' Files that are downloaded from IPEDS typically have a constant format within
+#' each type of table.
+#' The only difference among files is a year number.
+#' For example, a file for graduation rates from 1998 would be "GR1998.xlsx"
+#' 
+#' @param .path `<chr>` the path to search for. 
+#' @param .prefix `<chr>` the prefix for this type of table
+#' @param .suffix `<chr?>` an optional suffix to file names. Defaults to "" 
+#' @inheritDotParams base::dir -path -pattern 
+#'
+#' @returns `<chr[]>` a vector of file names, with names set to matching years
+#' @export
+find_yearly_tables <- function(.path, .prefix, .suffix = "", ...) {
+    
+    .files <- dir(.path,
+                  pattern = paste0(.prefix, "\\d{4}", .suffix),
+                  ...)
 
-find_yearly_tables <- function(.path, .prefix, .suffix) {
-    dir(.path,
-        pattern = paste0(.prefix, "\\d{4}", .suffix),
-        full.names = TRUE)
+    .yearmatches <- regexec("\\d{4}", .files)
+
+    names(.files) <- regmatches(.files, .yearmatches)
+    
+    .files
 }
